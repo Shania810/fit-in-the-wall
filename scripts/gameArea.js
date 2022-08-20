625421150fd747ba503cc7a224bdf6bcadacf153
Shania <shaniaantezana@gmail.com>
2022-08-20 08:18:01 -0400

    check GameOver,create page Game Over

--- scripts/gameArea.js
+++ scripts/gameArea.js
@@ -7,29 +7,22 @@ const triangle = {
     color: 'green',
     speedX:0,
     draw: function() {
-        const canvasElement = document.getElementById('canvas-stage');
-        const context = canvasElement.getContext("2d");
-        
-        // the triangle
-        context.beginPath();
-        context.moveTo(x,y);
-        context.lineTo(x,y + 50);
-        context.lineTo(x + 50,y + 50);
-        context.closePath();
-        
-        // the outline
-        context.lineWidth = 10;
-        context.strokeStyle = 'black';
-        context.stroke();
-        
-        // the fill color
-        context.fillStyle = "purple";
-        context.fill();
+     const canvasElement = document.getElementById('canvas-stage');
+     const context = canvasElement.getContext("2d");
+     context.beginPath();
+     context.moveTo(x,y);
+     context.lineTo(x,y + 50);
+     context.lineTo(x + 50,y + 50);
+     context.closePath();
+     context.lineWidth = 10;
+     context.strokeStyle = 'black';
+     context.stroke();
+     context.fillStyle = "purple";
+     context.fill();
     },
     newPosition : function(){
-        x += this.speedX
-     }
-       
+     return x += this.speedX
+    }
 }
 const square = {
     name: 'square',
@@ -40,44 +33,39 @@ const square = {
     color: 'black',
     speedX : 0,
     draw : function(){
-        const canvas = document.getElementById('canvas-stage');
-        const ctx = canvas.getContext('2d');
-        ctx.fillStyle = '#010D9A';
-        ctx.fillRect(x, y, 55, 55);
-        ctx.strokeStyle = 'black';
-        ctx.strokeRect(x, y, 55, 55);
-     },
+     const canvas = document.getElementById('canvas-stage');
+     const ctx = canvas.getContext('2d');
+     ctx.fillStyle = '#010D9A';
+     ctx.fillRect(x, y, 55, 55);
+     ctx.strokeStyle = 'black';
+     ctx.strokeRect(x, y, 55, 55);
+    },
      newPosition : function(){
-        x += this.speedX
-     }
-        
-    }
+     return x += this.speedX
+    }  
+}
 
 const circle = {
-name:'circle',
-width: 50,
-height: 50,
-speedX:0,
-color:'green',
-radius: 70,
-draw: function(){
-const canvas = document.getElementById('canvas-stage');
-const ctx = canvas.getContext('2d');
-const centerX = x ;
-const centerY = y;
-const radius = 38;
-
-ctx.beginPath();
-ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-ctx.fillStyle = 'green';
-ctx.fill();
-ctx.lineWidth = 5;
-ctx.strokeStyle = 'black';
-ctx.stroke();
-},
-newPosition : function(){
-    x += this.speedX
- }
+    name:'circle',
+    width: 50,
+    height: 50,
+    speedX:0,
+    color:'green',
+    radius: 70,
+    draw: function(){
+     const canvas = document.getElementById('canvas-stage');
+     const ctx = canvas.getContext('2d');
+     ctx.beginPath();
+     ctx.arc(x,y,38, 0, 2 * Math.PI, false);
+     ctx.fillStyle = 'green';
+     ctx.fill();
+     ctx.lineWidth = 5;
+     ctx.strokeStyle = 'black';
+     ctx.stroke();
+    },
+    newPosition : function(){
+     return x += this.speedX
+    }
 }
 const trapezoid ={
     name:'trapezoid',
@@ -85,169 +73,187 @@ const trapezoid ={
     width: 50,
     height: 20,
     draw : function (){
-            const canvas = document.getElementById('canvas-stage');
-            const ctx = canvas.getContext('2d');
-            ctx.beginPath();
-            ctx.moveTo(x,y); 
-            ctx.lineTo(x - 50, y ); 
-            ctx.lineTo(x - 90, y + 50);
-            ctx.lineTo(x + 50, y + 50);
-            ctx.lineTo(x + 1, y);
-            ctx.closePath();
-            
-            ctx.fillStyle = "red";
-            ctx.fill();
-            ctx.lineWidth = 5;
-            ctx.strokeStyle = 'black';
-            ctx.stroke();
+     const canvas = document.getElementById('canvas-stage');
+     const ctx = canvas.getContext('2d');
+     ctx.beginPath();
+     ctx.moveTo(x,y); 
+     ctx.lineTo(x - 50, y ); 
+     ctx.lineTo(x - 90, y + 50);
+     ctx.lineTo(x + 50, y + 50);
+     ctx.lineTo(x + 1, y);
+     ctx.closePath();      
+     ctx.fillStyle = "red";
+     ctx.fill();
+     ctx.lineWidth = 5;
+     ctx.strokeStyle = 'black';
+     ctx.stroke();
 
     },
     newPosition : function(){
-    return x += this.speedX
+     return x += this.speedX
     }
 }
-
 const myGame ={
-    player: null,
+    player: square,
     frames: 0,
     obstacule: [],
     score: 0 ,
+    gameOver:false,
     changePlayer: function(){
-        const forms = [square,triangle,trapezoid,circle]
-        const number = Math.floor(Math.random() * forms.length)
-        const randomForm = forms[number]
-        myGame.player = randomForm
+     switch (this.player.name) {
+        case 'square':
+            this.player = triangle
+            break;
+        case 'triangle':
+            this.player = trapezoid
+            break;
+        case 'trapezoid':
+            this.player = circle
+            break;
+        case 'circle':
+            this.player = square
+            break;
+        default:
+            break;
+        }
     },
     start: function(){
-        this.changePlayer()      
-        this.player.draw()
-        updateGame()
-        this.checkForm()
+     this.changePlayer()      
+     this.player.draw()
+     updateGame()
     },
     clear: function(){
-        const canvas = document.getElementById("canvas-stage");
-        const ctx = canvas.getContext("2d");
-        ctx.clearRect(0,0,1200,570)
+     const canvas = document.getElementById("canvas-stage");
+     const ctx = canvas.getContext("2d");
+     ctx.clearRect(0,0,1200,570)
     },
-    stop: function(){
-        cancelAnimationFrame(createObstacle)
+    checkForm: function(obstacle){
+     return this.player.name === obstacle.type
     },
-    checkForm: function(){
-        const obstacles = this.obstacule
-        obstacles.forEach((obstacle)=>{
-        console.log(obstacle.x)
-        console.log(obstacle.y)
-          if(this.player.name === 'triangle' && (x === obstacle.x + 10 || x < obstacle.x + 15 && x > obstacle.x - 15)&&(y.obstacle === 500 && y === 500)){
-            this.score += 1
-            console.log('ok')
-          }else{
-            this.score += 0
-            console.log('not')
-          }
-        })
-        
-        console.log(`score: ${this.score}`)
+    checkPosition : function(obstacle){
+     return x < obstacle.x+100 && x > obstacle.x-100
+    },
+    checkColision: function(obstacle){
+     return y<= obstacle.y+100
     }
 }
 class Obstacles{
-    constructor(x,type){
-       this.x = x;
-       this.y = 0;
-       this.speedY = 0; 
-       this.type = type;
+   constructor(x,type){
+     this.x = x;
+     this.y = 0;
+     this.speedY = 0; 
+     this.type = type;
     }
     drawWall(){
-    const  canvas = document.getElementById('canvas-stage')
-    const ctx = canvas.getContext('2d')
-     ctx.fillStyle = 'black'
+     const  canvas = document.getElementById('canvas-stage')
+     const ctx = canvas.getContext('2d')
+     ctx.fillStyle = 'blue'
      ctx.fillRect(0,this.y,1200,100);
     }
     drawCircle(){
-        const  canvas = document.getElementById('canvas-stage')
-        const ctx = canvas.getContext('2d')
-        ctx.beginPath();
-        ctx.arc(this.x + 50,this.y + 48, 40, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'white';
-        ctx.fill();
+     const  canvas = document.getElementById('canvas-stage')
+     const ctx = canvas.getContext('2d')
+     ctx.beginPath();
+     ctx.arc(this.x + 50,this.y + 48, 40, 0, 2 * Math.PI, false);
+     ctx.fillStyle = 'white';
+     ctx.fill();
     }
     drawTriangle(){
-        const  canvas = document.getElementById('canvas-stage')
-        const ctx = canvas.getContext('2d')
-        ctx.beginPath();
-        ctx.moveTo(this.x + 10,this.y + 10);
-        ctx.lineTo(this.x + 10,this.y + 80);
-        ctx.lineTo(this.x + 80,this.y + 80);
-        ctx.closePath();
-        ctx.fillStyle = 'white';
-        ctx.fill();
+     const  canvas = document.getElementById('canvas-stage')
+     const ctx = canvas.getContext('2d')
+     ctx.beginPath();
+     ctx.moveTo(this.x + 10,this.y + 10);
+     ctx.lineTo(this.x + 10,this.y + 80);
+     ctx.lineTo(this.x + 80,this.y + 80);
+     ctx.closePath();
+     ctx.fillStyle = 'white';
+     ctx.fill();
     }
     drawSquare(){
-        const canvas = document.getElementById('canvas-stage');
-        const ctx = canvas.getContext('2d');
-        ctx.fillStyle = 'white';
-        ctx.fillRect(this.x + 5 , this.y + 16, 65,65);
+     const canvas = document.getElementById('canvas-stage');
+     const ctx = canvas.getContext('2d');
+     ctx.fillStyle = 'white';
+     ctx.fillRect(this.x + 5 , this.y + 16, 65,65);
     }
     drawTrapezoid(){
-        const canvas = document.getElementById('canvas-stage');
-        const ctx = canvas.getContext('2d');
-            ctx.beginPath();
-            ctx.moveTo(this.x + 5,this.y + 15); 
-            ctx.lineTo(this.x - 80 , this.y + 15); 
-            ctx.lineTo(this.x - 120 , this.y + 70);
-            ctx.lineTo(this.x + 50 , this.y + 70);
-            ctx.lineTo(this.x + 1, this.y + 15);
-            ctx.closePath();
-            
-            ctx.fillStyle = 'white';
-            ctx.fill();
+     const canvas = document.getElementById('canvas-stage');
+     const ctx = canvas.getContext('2d');
+     ctx.beginPath();
+     ctx.moveTo(this.x + 5,this.y + 15); 
+     ctx.lineTo(this.x - 80 , this.y + 15); 
+     ctx.lineTo(this.x - 120 , this.y + 70);
+     ctx.lineTo(this.x + 50 , this.y + 70);
+     ctx.lineTo(this.x + 1, this.y + 15);
+     ctx.closePath();
+     ctx.fillStyle = 'white';
+     ctx.fill();
     }
     draw(){
-        this.drawWall()
-        if(this.type === 'triangle'){
-          this.drawTriangle()
-        }else if(this.type === 'circle'){
+     this.drawWall()
+      switch (this.type) {
+        case 'triangle':
+            this.drawTriangle()
+            break;
+        case 'circle':
             this.drawCircle()
-        }else if(this.type === 'square'){
+            break;
+        case 'square':
             this.drawSquare()
-        }
-        else if(this.type === 'trapezoid'){
+            break; 
+        case 'trapezoid':
             this.drawTrapezoid()
+            break;        
+        default:
+            break;
         }
     }
     move (){
-     this.y += this.speedY
+      return this.y += this.speedY
     }
 }
 function startGame(){
-    myGame.start();
+ myGame.start();
 }
 function updateGame(){
-    myGame.clear();
-    updateObstacle();
-    myGame.player.newPosition();
-    myGame.player.draw();
-    requestAnimationFrame(updateGame);
-    console.log(`posição do player ${x}`)
-    myGame.checkForm()
-}
-const forms = ['triangle','circle','square','trapezoid']
-function createObstacle(){
-    myGame.frames += 1;
-    if(myGame.frames % 600 === 0 ){
-    const randomForm = Math.floor(Math.random()*forms.length)
-    const randomPos = 60 + Math.floor(Math.random()*1000)
-    let obstacle = new Obstacles(randomPos,forms[randomForm]);
-    myGame.obstacule.push(obstacle);
+ myGame.clear();
+ updateObstacle();
+ myGame.player.newPosition();
+ myGame.player.draw();
+   for(let i = 0; i < myGame.obstacule.length; i++){
+    const checkColision = myGame.checkColision(myGame.obstacule[i])
+    if(checkColision){
+      const checkForm = myGame.checkForm(myGame.obstacule[i])
+      const checkPosition = myGame.checkPosition(myGame.obstacule[i])
+       myGame.obstacule.splice(i,1)
+       if(checkForm && checkPosition){
+       myGame.score +=1
+       }else{
+       myGame.gameOver = true
+       gameOver(myGame.score)
+        }
     }
+   }
+   if(!myGame.gameOver){
+    requestAnimationFrame(updateGame);
+   }
+}
+const forms =['square','triangle','trapezoid','circle'] 
+function createObstacle(){
+ myGame.frames += 1;
+ if(myGame.frames % 200 === 0 ){
+ const randomForm = Math.floor(Math.random()*forms.length)
+ const randomPos = 60 + Math.floor(Math.random()*1000)
+ let obstacle = new Obstacles(randomPos,forms[randomForm]);
+ myGame.obstacule.push(obstacle);
+ }
 }
  function updateObstacle(){
-  requestAnimationFrame(createObstacle)
-  myGame.obstacule.forEach((obstacle) =>{
-  obstacle.speedY += 0.001
+ requestAnimationFrame(createObstacle)
+ myGame.obstacule.forEach((obstacle) =>{
+  obstacle.speedY += 0.01
   obstacle.move()
   obstacle.draw()
-   })
-   
+})
 }
 
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,27 @@
-document.addEventListener('keydown',(e) =>{
-    const key = e.code;
-    if(key === 'Space') { 
-        myGame.changePlayer()    
-     }
-     
-    })
 document.addEventListener('keydown', (e) => {
     const key = e.code;
     switch (key) {
-        case 'ArrowLeft':
-        myGame.player.speedX -= 1;
-        break;
-        case 'ArrowRight':
-        myGame.player.speedX += 1;
-        break; 
+      case 'ArrowLeft':
+      myGame.player.speedX -= 1;
+      break;
+      case 'ArrowRight':
+      myGame.player.speedX += 1;
+      break; 
+      case 'Space':
+      myGame.changePlayer();
+      break;
     }
 })
 document.addEventListener('keyup', () => {
-    myGame.player.speedX = 0;
+  return myGame.player.speedX = 0;
 })
 window.onload = () => {
-    document.getElementById('start-game').addEventListener('click',start) 
+ document.getElementById('start-game').addEventListener('click',start) 
 }
+
 function start(){
-    document.getElementById('background').remove();
-    document.querySelector('html').innerHTML=`
+ document.getElementById('background').remove();
+ document.querySelector('html').innerHTML=`
     <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -33,11 +30,23 @@ function start(){
     <title>Fit In The Wall - game</title>
     <script src="./scripts/gameArea.js" defer></script>
     <script src="./scripts/index.js" defer ></script>
-</head>
-<body>
+   </head>
+   <body>
     <div>
     <canvas id="canvas-stage" width="1200" height="570"></canvas>
     </div>
-</body>`
-startGame();
+   </body>
+   `
+ startGame()
 }
+function gameOver(score){
+ document.querySelector('body').innerHTML = `
+    <div id="reset">
+      <div id="score">
+      <span>score: ${score}</span>
+      <button id="reset-game"><a href="index.html">Play Again</a></button>
+      </div>
+    </div>
+    `
+}
+
